86786e36d388ed528970f2682ba533d77e99e8dd
Rithik Raj <rithu0644@gmail.com>
2023-12-13 06:06:14 +0530

    Small fix up

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -3,5 +3,5 @@ from schemas.Users import Public, Worker, Business
 from schemas.Booking import BookPickupDetails
 from schemas.Reward import Reward
 from schemas.DailyLogs import logs, logsresponse
-from Schedule import Schedule
-from Post import Post
+from schemas.Schedule import Schedule
+from schemas.Post import Post
